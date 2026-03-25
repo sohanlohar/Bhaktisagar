d63fae8b12e73712f2f46ddd26788e13a5b400dd
sohanlohar <loharsonu11@gmail.com>
2026-03-25 16:23:41 +0530

    feat: Rename application to Bhakti Sangam and set up initial project files for Android and iOS.

--- android/app/src/main/java/com/bhaktisagar/MainActivity.kt
+++ android/app/src/main/java/com/bhaktisagar/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bhaktisagar
+package com.bhaktisangam
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
@@ -17,7 +17,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Bhaktisagar"
+  override fun getMainComponentName(): String = "BhaktiSangam"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/Bhaktisagar/AppDelegate.swift
+++ ios/Bhaktisagar/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "Bhaktisagar",
+      withModuleName: "BhaktiSangam",
       in: window,
       launchOptions: launchOptions
     )
